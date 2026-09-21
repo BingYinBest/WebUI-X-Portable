230190b802ba9fcdd92b2f41e85c5bd41bdff01a
BingYinBest <dreamfy66@outlook.com>
2026-09-21 15:51:08 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-val baseAppName = "汽水音乐"
+val baseAppName = "WebUI X"
 val mmrlBaseApplicationId = "com.dergoogler.mmrl"
 val basePackageName = "$mmrlBaseApplicationId.wx"
 
